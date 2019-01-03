ab0f5496fdbdd5d9d9028b57d2384c55e3878d24
Madhu Rimmalapudi <madhu.rimmalapudi@gmail.com>
2019-01-03 14:01:25 -0800

    Adding playing Card Model

--- PlayingCards/File.swift
+++ PlayingCards/File.swift
@@ -8,6 +8,44 @@
 
 import Foundation
 
-struct Card {
-    <#fields#>
+struct PlayingCard {
+    var suit : Suit
+    var rank : Rank
+    
+    enum Suit : String {
+        case spades = "♠️"
+        case hearts = "♥️"
+        case clubs  = "♣️"
+        case diamands = "♦️"
+        
+        static var allSuits = [Suit.spades,.hearts,.clubs,.diamands]
+        
+    }
+    
+    enum Rank {
+        case ace
+        case numeric(Int)
+        case face(String)
+        
+        var order : Int {
+            switch self {
+            case .ace: return 1
+            case .numeric(let pips) : return pips
+            case .face(let kind) where kind == "j" : return 11
+            case .face(let kind) where kind == "Q" : return 12
+            case .face(let kind) where kind == "K" : return 13
+            default: return 0
+            }
+        }
+        
+        static var all : [Rank] {
+            var allRanks = [Rank.ace]
+            for pips in 2...10 {
+                allRanks.append(Rank.numeric(pips))
+            }
+            allRanks += [Rank.face("j"),.face("Q"),.face("k")]
+            return allRanks
+        }
+        
+    }
 }
